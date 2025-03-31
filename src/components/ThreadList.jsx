1c011631f7f47ec100c2c699e21aa1be26d8eff8
candy023 <chitose0720@icloud.com>
2025-03-31 19:28:09 +0900

    Initial commit

---
+++ src/components/ThreadList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ThreadList() {
+  return (
+    <div className="thread-list">
+      <h2>スレッド一覧</h2>
+      <ul>
+        <li>スレッド1</li>
+        <li>スレッド2</li>
+        <li>スレッド3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ThreadList;
